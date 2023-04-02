fab217086e9c414ebe99898df8c52cda4fad5a4e
Jens <jens@schleusner.dev>
2023-04-02 12:10:45 +0000

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Resume v7.0.5 (https://hedgehawk.github.io/startbootstrap-resume)
-* Copyright 2013-2022 Start Bootstrap
+* Start Bootstrap - Resume v7.0.6 (https://hedgehawk.github.io/startbootstrap-resume)
+* Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
 //
